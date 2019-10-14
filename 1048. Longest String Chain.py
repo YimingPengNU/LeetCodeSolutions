17c3f17d702edf2c5990e6402798d63c7295fc52
Yiming Peng <ypeng@u.northwestern.edu>
2019-10-13 23:38:30 -0500

    Update 1048. Longest String Chain.py

--- 1048. Longest String Chain.py
+++ 1048. Longest String Chain.py
@@ -2,7 +2,6 @@ class Solution:
     def longestStrChain(self, words: List[str]) -> int:
         dpTable = {}
         for w in sorted(words, key=len):
-            dpTable[w] = max(dpTable.get(w[:i] + w[i+1:], 0) + 1 for i in range(len(w)))
-        
+            dpTable[w] = max(dpTable.get(w[:i] + w[i+1:], 0) + 1 for i in range(len(w)))      
         return max(dpTable.values())
             
