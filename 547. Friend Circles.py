8438462533959d86f5763f1e175db7d2cd7c8f1b
Yiming Peng <ypeng@u.northwestern.edu>
2019-10-13 23:37:37 -0500

    Update 547. Friend Circles.py

--- 547. Friend Circles.py
+++ 547. Friend Circles.py
@@ -2,18 +2,18 @@ class Solution:
     def findCircleNum(self, M: List[List[int]]) -> int:
         # DFS approach
         N = len(M)
-        nodes_visited = [False] * N
+        visited = [False] * N
         count = 0
         for i in range(N):
-            if not nodes_visited[i]:
-                count += 1
-                stack = [i]
-                while len(stack) > 0:
-                    curr = stack.pop()
-                    nodes_visited[curr]  = True
-                    adj_list = [index for index, item in enumerate(M[curr]) if item != 0 and not nodes_visited[index]]
-                    stack.extend(adj_list)
-                
+            if visited[i]:
+                continue
+            count += 1
+            self.dfs(M, i, visited)            
         return count
-                    
             
+    def dfs(self, M, i, visited):
+        visited[i] = True
+        for j in range(len(M[i])):
+            if M[i][j] == 1 and not visited[j]:
+                self.dfs(M, j, visited)
+     
