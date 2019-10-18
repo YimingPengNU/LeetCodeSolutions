86528ad2b5dd036240c0db3e0f31e6173c833fe5
Yiming Peng <ypeng@u.northwestern.edu>
2019-10-18 13:31:20 -0500

    Create 3. Longest Substring Without Repeating Characters.cpp

---
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() < 2)
+            return s.size();
+        int maxLen = 1;
+        int i = 0;
+        int j = 0;
+        set substr{s[i]};
+        while (i < s.size() && j < s.size()) {
+            if (substr.find(s[j+1]) == substr.end() && (j+1) < s.size()) {
+                j++;
+                substr.insert(s[j]);
+                int len = j - i + 1;
+                if (len > maxLen)
+                    maxLen = len;
+            }            
+            else {
+                substr.erase(s[i]);
+                i++;    
+            }                       
+        }
+        return maxLen;
+    }
+};
