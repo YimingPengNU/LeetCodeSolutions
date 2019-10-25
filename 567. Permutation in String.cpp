67a9ef91a6a687d0876d67b8f54d6785120175ed
Yiming Peng <ypeng@u.northwestern.edu>
2019-10-24 21:56:32 -0500

    Create 567. Permutation in String.cpp

---
+++ 567. Permutation in String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size())
+            return false;
+        vector<int> s1map(26,0);
+        vector<int> s2map(26,0);
+        for (int i = 0; i < s1.size(); ++i) {
+            s1map[s1[i] - 'a']++;
+            s2map[s2[i] - 'a']++;
+        }
+        for (int i = 0; i < s2.size() - s1.size(); ++i) {
+            if (match(s1map, s2map))
+                return true;
+            s2map[s2[i + s1.size()] - 'a']++;
+            s2map[s2[i] - 'a']--;
+        }
+        return match(s1map, s2map);
+    }  
+    
+    bool match(vector<int> &s1map, vector<int> &s2map) {
+        for (int i = 0; i < s1map.size(); ++i) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+};
