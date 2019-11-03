56934f5aa85d89a07aa64c8c6afe9b6459d112b5
Yiming Peng <ypeng@u.northwestern.edu>
2019-11-02 19:37:53 -0500

    Create 347. Top K Frequent Elements.py

---
+++ 347. Top K Frequent Elements.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = collections.Counter(nums)   
+        return heapq.nlargest(k, count.keys(), key=count.get) 
+        
