cb45d881f4c855c5ff45833c458f092049ba58d6
Yiming Peng <ypeng@u.northwestern.edu>
2019-11-30 00:43:10 -0600

    Create 51. N-Queens.py

---
+++ 51. N-Queens.py
@@ -0,0 +1,19 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        return self.placeQueens(n)
+    
+    def placeQueens(self, n, r=0, Q=None, res=[]):
+        if not Q: 
+            Q, res = [0] * n, []
+        if r == n:
+            res.append(['.' * c + 'Q' + '.' * (n - c - 1) for c in Q])
+            return
+        for j in range(n):
+            legal = True
+            for i in range(r):
+                if Q[i] == j or Q[i] == j+r-i or Q[i] == j-r+i:
+                    legal = False
+            if legal:
+                Q[r] = j
+                self.placeQueens(n, r+1, Q, res)
+        return res
